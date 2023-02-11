cf29cd748d50f395e0f361e7edde1e902ffd072e
itsuki-jp <itsuki19601@gmail.com>
2023-02-11 23:14:11 +0900

    2022-02-11

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,16 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = userList.map(test);
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(result);
+    }, 3000);
+  })
 }
 
-function test() {
-  return
+function test(data) {
+  data.full_name = `${data.family_name} ${data.first_name}`
+  return data;
 }
 
